2b2f2ead1f685a9efb28ee2c0ff3cbe452a724dd
eidheim <eidheim@gmail.com>
2018-09-28 08:33:09 +0200

    Added upcoming LLVM release

--- cmake_modules/FindLibClang.cmake
+++ cmake_modules/FindLibClang.cmake
@@ -14,7 +14,7 @@
 #               Known LLVM release numbers.
 
 # most recent versions come first
-set(LIBCLANG_KNOWN_LLVM_VERSIONS 7.0.0 7.0 7
+set(LIBCLANG_KNOWN_LLVM_VERSIONS 7.0.1 7.0.0 7.0 7
   6.0.1 6.0.0 6.0 6
   5.0.1 5.0.0 5.0 5
   4.0.1
